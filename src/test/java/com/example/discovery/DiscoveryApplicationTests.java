1bf5871a65627268a3741c9a5ff83c6ef5cbd5a6
Pawel <pawel.tomczyk85@gmail.com>
2021-06-28 18:10:13 +0200

    init

--- src/test/java/com/example/discovery/DiscoveryApplicationTests.java
+++ src/test/java/com/example/discovery/DiscoveryApplicationTests.java
@@ -2,8 +2,11 @@ package com.example.discovery;
 
 
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootTest
 class DiscoveryApplicationTests {
